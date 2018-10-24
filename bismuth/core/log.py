2303c1cb0404e5344f1b81c87e0f2fb8e3e8dbcc
Isaac Cook <isaac@simpload.com>
2018-10-24 02:28:13 -0500

    Add color coding to log.py

--- bismuth/core/log.py
+++ bismuth/core/log.py
@@ -13,6 +13,43 @@ def filter_status(record):
     else:
         return 0
 
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+
+#The background is set with 40 plus the number of the color, and the foreground with 30
+
+#These are the sequences need to get colored ouput
+RESET_SEQ = "\033[0m"
+COLOR_SEQ = "\033[1;%dm"
+BOLD_SEQ = "\033[1m"
+
+def formatter_message(message, use_color = True):
+    if use_color:
+        message = message.replace("$RESET", RESET_SEQ).replace("$BOLD", BOLD_SEQ)
+    else:
+        message = message.replace("$RESET", "").replace("$BOLD", "")
+    return message
+
+COLORS = {
+    'WARNING': YELLOW,
+    'INFO': WHITE,
+    'DEBUG': BLUE,
+    'CRITICAL': YELLOW,
+    'ERROR': RED
+}
+
+class ColoredFormatter(logging.Formatter):
+    def __init__(self, msg, use_color = True):
+        logging.Formatter.__init__(self, msg)
+        self.use_color = use_color
+
+    def format(self, record):
+        levelname = record.levelname
+        if self.use_color and levelname in COLORS:
+            levelname_color = COLOR_SEQ % (30 + COLORS[levelname]) + levelname + RESET_SEQ
+            record.levelname = levelname_color
+        return logging.Formatter.format(self, record)
+
+
 
 def log(logFile, level_input="WARNING", terminal_output=False):
     if level_input == "NOTSET":
@@ -46,7 +83,9 @@ def log(logFile, level_input="WARNING", terminal_output=False):
         # No need for complete func and line info here.
         formatter = logging.Formatter('%(asctime)s %(message)s')
     else:
-        formatter = logging.Formatter('%(asctime)s %(funcName)s(%(lineno)d) %(message)s')
+        FORMAT = "[$BOLD%(name)-10s$RESET][%(levelname)-18s]  %(message)s ($BOLD%(filename)s$RESET:%(lineno)d)"
+        COLOR_FORMAT = formatter_message(FORMAT, True)
+        formatter = ColoredFormatter(COLOR_FORMAT)
     ch.setFormatter(formatter)
     app_log.addHandler(ch) 
 
